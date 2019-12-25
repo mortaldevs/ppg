9ec3a88717aff5aa0c33fb754f9cc5f556214f25
Mortaldevs <you@example.com>
2019-12-25 19:24:11 +0500

    Title updated

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="images/favicon.png" sizes="16x16" type="image/png">
-    <title>Privacy Policy Ganerator</title>
+    <title>Privacy Policy Ganerator - Free </title>
     <link rel="stylesheet" href="styles.css">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
